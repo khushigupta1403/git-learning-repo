8e96981b994fe2be0126f86367594d0a9f341e56
khushigupta1403 <khushigupta25nov@gmail.com>
2025-05-22 09:12:36 +0530

    Add question7 between1and10 function

---
+++ JAVASCRIPT/FUNCTIONS/question7_between1and10.js
@@ -0,0 +1,7 @@
+//Write a function that takes a number as input and determines if it is between 1 and 10
+function between1and10(a){
+    return a>=1  && a<=10;
+}
+console.log(between1and10(5));
+console.log(between1and10(100));
+console.log(between1and10(9));
