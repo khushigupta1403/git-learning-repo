0a7912d92b3d2114afa94d08496e8f39df690f0e
khushigupta1403 <khushigupta25nov@gmail.com>
2025-05-22 09:21:20 +0530

    Add question10 multiplying_by10 function

---
+++ JAVASCRIPT/FUNCTIONS/question10_multiplying_by10.js
@@ -0,0 +1,9 @@
+//Write a function which takes in a number as input and returns it after multiplying by 10.
+function multiplyByTen(num) {
+    return num * 10;
+}
+
+console.log(multiplyByTen(2));    
+console.log(multiplyByTen(80));   
+console.log(multiplyByTen(-3));   
+console.log(multiplyByTen(1));    
