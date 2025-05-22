02a1af73ffb8b5644b4f2cea2e7107f7aa081dcf
khushigupta1403 <khushigupta25nov@gmail.com>
2025-05-22 08:53:02 +0530

    Add question1 voting function

---
+++ JAVASCRIPT/STRING/JAVASCRIPT/FUNCTIONS/question1_Voting.js
@@ -0,0 +1,11 @@
+// Write a function that takes person age as a number as input and determines, person are eligible or not for voting
+function voting(age) {
+    if (age >= 18) {
+        return "You are eligible to vote.";
+    } else {
+        return "You are not eligible to vote.";
+    }
+}
+
+console.log(voting(20));Get-ChildItem -Recurse
+
