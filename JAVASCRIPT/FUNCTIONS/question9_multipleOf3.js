04e1e98ed5d59bf18c14e626c2654ed3de02f0e8
khushigupta1403 <khushigupta25nov@gmail.com>
2025-05-22 09:19:03 +0530

    Add question9 multipleOf3 function

---
+++ JAVASCRIPT/FUNCTIONS/question9_multipleOf3.js
@@ -0,0 +1,7 @@
+//  Write a function that takes a number as input and determines if it is a multiple of 3
+function multipleof3(x){
+    return x%3===0;
+}
+console.log(multipleof3(56));
+console.log(multipleof3(99));
+console.log(multipleof3(7));
