9674f3e89ae61459df89c94e1e03b691f1581cd0
khushigupta1403 <khushigupta25nov@gmail.com>
2025-05-22 09:16:00 +0530

    Add question8 hello function

---
+++ JAVASCRIPT/FUNCTIONS/question8_hello.js
@@ -0,0 +1,8 @@
+//Write a function that takes a string as input and determines if it contains the word "hello"
+function Hello(str){
+    return str.includes("hello");
+}
+console.log(Hello("hello alpha intern"));
+console.log(Hello("Hello alpha intern"));
+
+    
